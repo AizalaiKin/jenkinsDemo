253d0ddb2f9ff1bc5f3af266351380e4de4e7c80
aiyadogdurbek <frf_lfl@mail.ru>
2019-07-15 20:40:21 -0500

    June15JENKINS

---
+++ src/test/java/JenkinsJuly15.java
@@ -0,0 +1,13 @@
+public class JenkinsJuly15 {
+
+    public static void main(String[] args) {
+
+//
+//        String str = "I am perfect in JENKINS";
+//
+//        for (int i=0;i<= str.length();i++){
+//            System.out.println( str);
+//        }
+//
+  }
+}
